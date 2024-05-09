75c07a21fc29f3570017445889727a95ce6edd58
satyamrkushwaha <satyamkushwahasrk95@gmail.com>
2024-05-09 23:45:26 +0530

    [update] Constants move to the separate constant file in constant folder

---
+++ src/constants/constants.js
@@ -0,0 +1,126 @@
+export const minExpOptions = [
+    {
+      label: '1 Year',
+      value: 1,
+    },
+    {
+      label: '2 Years',
+      value: 2,
+    },
+    {
+      label: '3 Years',
+      value: 3,
+    },
+    {
+      label: '4 Years',
+      value: 4,
+    },
+    {
+      label: '5 Years',
+      value: 5,
+    },
+    {
+      label: '6 Years',
+      value: 6,
+    },
+    {
+      label: '7 Years',
+      value: 7,
+    },
+    {
+      label: '8 Years',
+      value: 8,
+    },
+    {
+      label: '9 Years',
+      value: 9,
+    },
+    {
+      label: '10 Years',
+      value: 10,
+    },
+  ];
+
+
+  export  const minPayOptions = [
+    {
+      label: '1 LPA',
+      value: 1,
+    },
+    {
+      label: '10 LPA',
+      value: 10,
+    },
+    {
+      label: '20 LPA',
+      value: 20,
+    },
+    {
+      label: '30 LPA',
+      value: 30,
+    },
+    {
+      label: '40 LPA',
+      value: 40,
+    },
+    {
+      label: '50 LPA',
+      value: 50,
+    },
+    {
+      label: '60 LPA',
+      value: 60,
+    },
+    {
+      label: '70 LPA',
+      value: 70,
+    },
+    {
+      label: '80 LPA',
+      value: 80,
+    },
+    {
+      label: '90 LPA',
+      value: 90,
+    },
+    {
+      label: '100 LPA',
+      value: 100,
+    },
+  ];
+
+
+
+  export  const engineeringOptions = [
+    {
+      label: 'Frontend',
+      value: 'frontend',
+    },
+    {
+      label: 'Backend',
+      value: 'backend',
+    },
+    {
+      label: 'Andriod',
+      value: 'android',
+    },
+    {
+      label: 'Ios',
+      value: 'ios',
+    },
+    {
+      label: 'Tech Lead',
+      value: 'tech lead',
+    },
+  ];
+
+  export  const groupedOptions = [
+    {
+      label: 'ENGINEERING',
+      options: engineeringOptions,
+    },
+  ];
+
+
+
+
